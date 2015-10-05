39e6eaf588ba6517f3d26837c56b7d1c64239148
Gustavo Nery <gustavo.nery@br.ibm.com>
2015-10-05 13:42:12 -0300

    merge with ibm git

--- client/app/app.js
+++ client/app/app.js
@@ -16,34 +16,41 @@ require('./components/play');
 
 //Define routes, views and controllers
 app.config(function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/play');
-        $stateProvider
-            .state('play', {
-                abstract: true,
-                url: '/play',
-                template: '<div ui-view></div>'
-            })
-            .state('play.connect', {
-                url: '',
-                templateUrl: 'app/components/play/connect/connectView.html',
-                controller: 'ConnectController'
-            })
-            .state('play.device', {
-                url: '/play/device',
-                templateUrl: 'app/components/play/device/deviceView.html',
-                controller: 'DeviceController'
-            })
-            .state('play.deviceData', {
-                url: '/play/devicedata',
-                templateUrl: 'app/components/play/deviceData/deviceDataView.html',
-                controller: 'DeviceDataController'
-            })
-            .state('play.deviceDataExtend', {
-                url: '/play/devicedataextend',
-                templateUrl: 'app/components/play/deviceDataExtend/deviceDataExtendView.html',
-                controller: 'DeviceDataExtendController'
-            });
-    })
-    .controller('MainController', function($scope, $state) {
-        $scope.stateIncludes = $state.includes;
+    $urlRouterProvider.otherwise('/play');
+    $stateProvider
+      .state('play', {
+        abstract: true,
+        url: '/play',
+        template: '<div ui-view></div>'
+      })
+      .state('play.connect', {
+        url: '',
+        templateUrl: 'app/components/play/connect/connectView.html',
+        controller: 'ConnectController'
+      })
+      .state('play.device', {
+        url: '/play/device',
+        templateUrl: 'app/components/play/device/deviceView.html',
+        controller: 'DeviceController'
+      })
+      .state('play.deviceData', {
+        url: '/play/devicedata',
+        templateUrl: 'app/components/play/deviceData/deviceDataView.html',
+        controller: 'DeviceDataController'
+      })
+      .state('play.deviceDataExtend', {
+        url: '/play/devicedataextend',
+        templateUrl: 'app/components/play/deviceDataExtend/deviceDataExtendView.html',
+        controller: 'DeviceDataExtendController'
+      });
+  })
+  // redirect engine
+  .run(function($rootScope, $state) {
+
+    $rootScope.$on('$stateChangeStart', function(evt, to, params) {
+      if (to.redirectTo) {
+        evt.preventDefault();
+        $state.go(to.redirectTo, params);
+      }
     });
+  });
